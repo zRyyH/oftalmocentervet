8c43b7d93d22d213c262fbb99838e3fb174fd686
zRyyH <zryyh.br@gmail.com>
2026-02-03 13:55:19 -0300

    v1.0

--- Robo/FinpetSimplesvet/__init__.py
+++ Robo/FinpetSimplesvet/__init__.py
@@ -1,5 +1,4 @@
-from .principal import executar_finpet_lancamentos
-from .planilha import gerar_relatorio
+from .principal import executar_finpet_lancamentos, gerar_relatorio
 from .vinculador import vincular
 
 __all__ = ["vincular", "gerar_relatorio", "executar_finpet_lancamentos"]
